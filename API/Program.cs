12f5f0a9376608ac3c61bda6eaefcf2de21b7920
Martin Frajkor <martin.frajkor@student.tuke.sk>
2022-08-19 13:36:41 +0200

    adding back cached files

---
+++ API/Program.cs
@@ -0,0 +1,63 @@
+using API.Data;
+using API.Extensions;
+using API.Middleware;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+var AllowSpecificOrigins = "_allowSpecificOrigins";
+
+// Add services to the container.
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowSpecificOrigins,
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    });
+});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//SEEDING THE DATABASE
+// using var scope = app.Services.CreateScope();
+// var services = scope.ServiceProvider;
+// tryy
+// {
+//     var context = services.GetRequiredService<DataContext>();
+//     await context.Database.MigrateAsync();
+//     await Seed.SeedUsers(context);
+// }
+// catch (Exception ex)
+// {
+//     var logger = services.GetRequiredService<ILogger<Program>>();
+//     logger.LogError(ex, "An error occured during migration");
+// }
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(AllowSpecificOrigins);
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+app.Run();
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'UdemyAsp';
+  users: any;
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    axios
+      .get('https://localhost:7026/api/users')
+      .then((res) => {
+        this.users = res.data;
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+}

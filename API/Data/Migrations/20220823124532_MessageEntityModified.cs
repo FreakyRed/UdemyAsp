01a64e3b2f56a1722ae414b3af10e0aaf59335e4
Martin Frajkor <martin.frajkor@student.tuke.sk>
2022-08-23 16:08:16 +0200

    messages implementation of backend

---
+++ API/Data/Migrations/20220823124532_MessageEntityModified.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class MessageEntityModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RecipientUsername",
+                table: "Messages",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipientUsername",
+                table: "Messages",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
